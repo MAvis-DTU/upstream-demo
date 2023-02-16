e2bf1fc2588c7358179569eea649da4723609287
AntonJorg <anton2410@gmail.com>
2023-02-16 10:18:46 +0100

    adds Foo

---
+++ foo.py
@@ -0,0 +1,7 @@
+
+class Foo:
+    def __init__(self, x):
+        self.x = x
+
+    def __repr__(self):
+        return f"Foo[x={self.x}]"
---
+++ main.py
@@ -0,0 +1,6 @@
+from foo import Foo
+
+if __name__ == "__main__":
+    f = Foo(42)
+    print(f)
+
